e096884f4b4ede5494e2a4c5371854b8fc45ed73
Santana-Eh <vargasa41@mail.sacredheart.edu>
2016-10-06 02:06:18 -0400

    Add files via upload

---
+++ ProjectEulerSantanaCardilloQ1.py
@@ -0,0 +1,19 @@
+result = 0
+for i in range(0,1000):
+ if (i % 3 == 0 or i % 5 == 0):
+  print i
+  result = result + i
+  print result
+
+import unittest
+
+
+class Mytest(unittest.TestCase):
+    def testAdd(self):
+        self.assertEqual((i % 3), 0)
+        self.assertEqual((i % 5), 4)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
