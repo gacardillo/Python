1abfb88d0b7543d7c56f74d4d609c091f1f69517
gacardillo <gacardillo@yahoo.com>
2016-11-16 19:14:55 -0800

    FinalProject-login

---
+++ urls.py
@@ -0,0 +1,28 @@
+"""untitled4 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib.sites import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+
+    # user auth urls
+    url(r'^accounts/login/$',  'django_test.views.login'),
+    url(r'^accounts/auth/$',   'django_test.views.auth_view'),
+    url(r'^accounts/logout/$', 'django_test.views.logout'),
+    url(r'^accounts/loggedin/$', 'django_test.views.loggedin'),
+    url(r'^accounts/invalid/$', 'django_test.views.invalid_login'),
+]
