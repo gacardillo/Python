b1a50d6b8c8d4bb581ec2d2149fd2dfea9560d93
gacardillo <gacardillo@yahoo.com>
2016-11-23 15:12:36 -0800

    Invalid_login.py

---
+++ invalid_login.html.py
@@ -0,0 +1,8 @@
+{% extends "base.html" %}
+
+{% block content %}
+
+    <h2>Login username and password are invalid</h2>
+    <p>Click <a href="/accounts/login/">here</a> to attempt again.</p>
+
+{% endblock %}
